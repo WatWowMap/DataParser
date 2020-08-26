af3a4b99265e91d1ae7e391a454ba60292ed0611
versx <versx@ver.sx>
2020-08-25 23:52:26 -0700

    Remove device controller route

--- src/index.js
+++ src/index.js
@@ -30,7 +30,6 @@ if (cluster.isMaster) {
 
     app.get('/', (req, res) => res.send('OK'));
     app.post('/raw', async (req, res) => await routes.handleRawData(req, res));
-    app.post('/controler', async (req, res) => await routes.handleControllerData(req, res));
 
     app.listen(config.port, config.host, () => console.log(`Listening on ${config.host}:${config.port}...`));
 }
